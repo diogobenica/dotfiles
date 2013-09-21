ebcb4dce4738cb5ae8a468dba03802a430c8a3ed
Diogo Benicá <diogobenica@gmail.com>
2013-09-20 21:20:15 -0300

    Friendly message and copy iTerm preferences if it's not open

--- setup.sh
+++ setup.sh
@@ -11,23 +11,31 @@ olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bash_aliases bash_colors bash_exports bash_profile bashrc gemrc gitconfig irbrc"    # list of files/folders to symlink in homedir
 
 ##########
+echo "I'll move your actual dotfiles to ~/dotfiles_old"
+echo "After that, I'm going to make symlinks to ~/ of the dotfiles"
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/.$file ~/.$file
+    mv ~/.$file $olddir/ 2> /dev/null
+    ln -s $dir/.$file ~/.$file 2> /dev/null
 done
+echo "Dotfiles done!"
+
+# copy files
+echo "Now I'll copy some preferences files"
+ITERM_RUNNING=$(ps -ef | grep iTerm | grep -cv grep)
+if [[ ($ITERM_RUNNING > 0) ]]; then
+  echo "[error] iTerm profile file wasn't copied"
+  echo "******** Close iTerm and run from Terminal.app to update the profile file ********"
+else
+  rm ~/Library/Preferences/com.googlecode.iterm2.plist
+  ln -s $dir/files/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+fi
 
 source ~/.bash_profile
